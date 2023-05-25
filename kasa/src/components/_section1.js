36fd5ee41091f45451a8686614465484b1b7221f
M-BEDH <mel.b.ms@pm.me>
2023-05-25 16:04:05 +0200

    Modifications section1-js, index-js, Cart-css

--- kasa/src/components/_section1.js
+++ kasa/src/components/_section1.js
@@ -1,13 +1,12 @@
-import IMG from '../assets/IMG.png'
-import '../styles/Home.css'
+import '../styles/_section1.css'
 
-function Section1() {
+function Section1({ img, titre }) {
   return (
     <div className="background">
       <h2 className="section1">
         {' '}
-        <img src={IMG} alt="paysage bord de mer" className="section1 img" />
-        <p className="text">Chez vous, partout et ailleurs</p>{' '}
+        <img src={img} alt="paysage bord de mer" className="section1 img" />
+        <p className="text">{titre}</p>{' '}
       </h2>
       <style>
         @import
--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
-import App from './pages/Home/index'
+import App from './App'
 import reportWebVitals from './reportWebVitals'
+//import App from './pages/Erreur404/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
