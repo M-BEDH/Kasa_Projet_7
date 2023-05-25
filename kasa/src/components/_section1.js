a6b4963a0a332ac6a28251d4e327e5bd99fbc46a
M-BEDH <mel.b.ms@pm.me>
2023-05-25 10:22:15 +0200

    _section1

---
+++ kasa/src/components/_section1.js
@@ -0,0 +1,20 @@
+import IMG from '../assets/IMG.png'
+import '../styles/Home.css'
+
+function Section1() {
+  return (
+    <div className="background">
+      <h2 className="section1">
+        {' '}
+        <img src={IMG} alt="paysage bord de mer" className="section1 img" />
+        <p className="text">Chez vous, partout et ailleurs</p>{' '}
+      </h2>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap');
+      </style>
+    </div>
+  )
+}
+
+export default Section1
