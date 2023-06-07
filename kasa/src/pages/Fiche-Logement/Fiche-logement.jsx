dc7060e94cd3e9a3c2e167a51d9e330990b73aba
M-BEDH <mel.b.ms@pm.me>
2023-06-08 01:13:11 +0200

    Modif page fiche Logement

--- kasa/src/pages/Fiche-Logement/Fiche-logement.jsx
+++ kasa/src/pages/Fiche-Logement/Fiche-logement.jsx
@@ -6,57 +6,62 @@ import '../../styles/FicheLogement.css'
 import Collapse from '../../components/Collapse'
 import Carroussel from '../../components/Carroussel'
 
- 
- function FicheLogement() {
+function FicheLogement() {
         const params = useParams()
         const logement = LogementsList.find(logement => logement.id === params.id)
-        const equipements = (logement.equipments)
+        const equipments = (logement.equipments)
         const description = (logement.description)
-        const images = (logement.pictures) 
-        for (let i = 0; i < images.length; i++)
-        console.log('fiche logement images :'+ images[i])
+        
+        const tags = (logement.tags)
+       
+      
         const stars = (logement.rating)
         const name = (logement.host).name
-        const tags =  (logement.tags)
-        for (let j = 0; j < tags.length; j++)
-        console.log( 'Fiche logement tags : ' +tags[j])
-        
+        let  images= (logement.pictures)
+        for (let j = 0; j < images.length; j++)
+        for (let i = 0; i < tags.length; i++)
+         
         return (
                 <div>
-                < Carroussel
-                pictures={logement.pictures[0]}
+                < Carroussel 
+                images= {images}
                 />
-                
                 < Body
                 titre={logement.title}
                 location={logement.location}
                 stars={stars}
                 name={name}
-                picture={logement.host.picture}              
-                tags={tags[0]}
-                />
-                
-                <div className='information'>
-                <div className='description'>
-                < Rectangle
-                titre={'Description'} />
-                < Collapse
-                titre={'Description'}
-                texte={description} />
-                </div>
-                
-                <div className='description'>
-                < Rectangle
-                titre={'Equipements'} />
-                < Collapse
-                titre={'Equipements'}
-                texte={equipements} />
-                </div>
-                </div>
-                
-                </div>
-                )
-        }
-        export default FicheLogement
-        
-        
+                profilePicture={logement.host.picture}  
+                tags={tags.map((tags, index) => (
+                    <div className='tagsFL'> 
+                        <p key={index.tags}>{tags} </p>
+                 </div>
+                        ))}
+                        />
+                        
+                        <div className='information'>
+                        <div className='description'>
+                        < Rectangle
+                        titre={'Description'} />
+                        < Collapse
+                        titre={'Description'}
+                        texte={description} />
+                        </div>
+                        
+                        <div className='description'>
+                        < Rectangle
+                        titre={'Equipements'} />
+                        < Collapse
+                        titre={'Equipements'}
+                        texte={equipments.map((equipments, index) => 
+                                <div key={index.equipments}> {equipments} </div>     
+                                )} />
+                                </div>
+                                </div>
+                                
+                                </div>
+                                )
+                        }
+                        export default FicheLogement
+                        
+                        
