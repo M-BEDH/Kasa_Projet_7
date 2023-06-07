fbe05df2483f5404305e65cf391f5da90471b758
M-BEDH <mel.b.ms@pm.me>
2023-06-07 21:12:11 +0200

    Modif page fiche logement

--- kasa/src/pages/Fiche-Logement/Fiche-logement.jsx
+++ kasa/src/pages/Fiche-Logement/Fiche-logement.jsx
@@ -1,47 +1,62 @@
- import {useParams} from 'react-router-dom'
- import Body from '../../components/_body'
- import LogementsList from '../../data/myJson.json'
- import Rectangle from '../../components/_rectangles'
- import '../../styles/FicheLogement.css'
- import Collapse from '../../components/Collapse'
+import {useParams} from 'react-router-dom'
+import Body from '../../components/_body'
+import LogementsList from '../../data/myJson.json'
+import Rectangle from '../../components/_rectangles'
+import '../../styles/FicheLogement.css'
+import Collapse from '../../components/Collapse'
+import Carroussel from '../../components/Carroussel'
+
  
-function FicheLogement() {
+ function FicheLogement() {
         const params = useParams()
         const logement = LogementsList.find(logement => logement.id === params.id)
         const equipements = (logement.equipments)
         const description = (logement.description)
+        const images = (logement.pictures) 
+        for (let i = 0; i < images.length; i++)
+        console.log('fiche logement images :'+ images[i])
+        const stars = (logement.rating)
+        const name = (logement.host).name
+        const tags =  (logement.tags)
+        for (let j = 0; j < tags.length; j++)
+        console.log( 'Fiche logement tags : ' +tags[j])
         
         return (
                 <div>
+                < Carroussel
+                pictures={logement.pictures[0]}
+                />
                 
-                        < Body
-                                titre={logement.title}
-                                location={logement.location}
-                                rate={logement.rating}
-                                Host={logement.host}
-                        />
+                < Body
+                titre={logement.title}
+                location={logement.location}
+                stars={stars}
+                name={name}
+                picture={logement.host.picture}              
+                tags={tags[0]}
+                />
                 
-                        <div className='information'>
-                                <div className='description'>
-                                        < Rectangle
-                                                titre={'Description'} />
-                                        < Collapse
-                                                titre={'Description'}
-                                                texte={description} />
-                                </div>
+                <div className='information'>
+                <div className='description'>
+                < Rectangle
+                titre={'Description'} />
+                < Collapse
+                titre={'Description'}
+                texte={description} />
+                </div>
                 
-                                <div className='description'>
-                                        < Rectangle
-                                                titre={'Equipements'} />
-                                        < Collapse
-                                                titre={'Equipements'}
-                                                texte={equipements} />
-                                </div>
-                        </div>
+                <div className='description'>
+                < Rectangle
+                titre={'Equipements'} />
+                < Collapse
+                titre={'Equipements'}
+                texte={equipements} />
+                </div>
+                </div>
                 
-   </div>
-        )
-}
+                </div>
+                )
+        }
         export default FicheLogement
         
         
